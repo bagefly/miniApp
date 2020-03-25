fcb499dc360556d0a5eba9eeef032270890b247b
bagefly <huangxhe@126.com>
2020-03-25 11:40:08 +0800

    user

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello Ask',
+    motto: 'Hello User',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
